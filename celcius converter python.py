720efe3b202697ca32f10daa2051f54e777cbd6e
Samme Qandil <Saqandil@gmail.com>
2017-09-14 16:57:14 -0700

    Update celcius converter python.py

--- celcius converter python.py
+++ celcius converter python.py
@@ -1,7 +1,7 @@
 #practicing list comprehension
 #Cel means celsius
 #Far means fahrenheit
-Cel = [18.7, 36.3, 43.1, 9.7, 37.4]
+Cel = [18.7, 36.3, 43.1, 9.7, 37.4] # this part here can be subbed with any celsius temperature(s).
 Far = [ ((float)(9)/5)*x + 32 for x in Cel] # this eqaution here, go to next line,
 print (Far) # expresses the C x (9/5) + 32 = F. Must remeber to add in that float because computers are dumb.
 
